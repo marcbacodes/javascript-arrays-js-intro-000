1b21febf9304558ba121e44c6fe7fc7562480bc1
Marc BA <marc.bisbalarias@dowjones.com>
2019-10-08 13:31:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
 
-function addElementToBeginningOfArray(chocolateBars,exercise){
-chocolateBars.unshift(mars);
+function addElementToBeginningOfArray(chocolateBars){
+chocolateBars.unshift("mars");
 return chocolateBars;
 }
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars,mars){
-chocolateBars.unshift(mars);
+function destructivelyAddElementToBeginningOfArray(chocolateBars){
+chocolateBars.unshift("mars");
 return chocolateBars;
 }
 
